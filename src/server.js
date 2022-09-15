f4ea9623685c72e965d7ed6f1c701503c02d7868
barianbarbar <barianbarbar99@gmail.com>
2022-09-15 21:43:42 +0700

    update add songs feature

--- src/server.js
+++ src/server.js
@@ -1,12 +1,18 @@
 require("dotenv").config();
 const Hapi = require("@hapi/hapi");
+
 const albums = require("./api/Albums");
 const AlbumsService = require("./services/AlbumService");
 const AlbumsValidator = require("./validator/Albums");
 
+const songs = require("./api/Songs");
+const SongsService = require("./services/SongsService");
+const SongsValidator = require("./validator/Songs");
+
 const init = async () => {
 
   const albumsService = new AlbumsService();
+  const songsService = new SongsService();
 
   const server = Hapi.server({
     port: process.env.PORT,
@@ -23,6 +29,14 @@ const init = async () => {
     options: {
       service: albumsService,
       validator: AlbumsValidator,
+    },
+  });
+
+  await server.register({
+    plugin: songs,
+    options: {
+      service: songsService,
+      validator: SongsValidator,
     },
   });
 

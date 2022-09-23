af2947adb49b0879c81cdca350155520dcc47a5f
radearsr <radea.surya4@gmail.com>
2022-09-24 06:10:11 +0700

    testing commit

--- src/exceptions/AuthenticationError.js
+++ src/exceptions/AuthenticationError.js
@@ -3,7 +3,7 @@ const ClientError = require("./ClientError");
 class AuthenticationsError extends ClientError {
   constructor(message) {
     super(message, 401);
-    this.name = "Authentication Error";
+    this.name = "Authentication Errors";
   }
 }
 
